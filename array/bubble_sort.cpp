f1483de6028830e7c5d8dec6c015e8123c49bb2f
avneet <avneetk1312@gmail.com>
2022-02-15 00:22:29 +0530

    selection and bubble sort

---
+++ array/bubble_sort.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int array[n];
+    for(int i=0;i<n;i++){
+        cin>>array[i];
+    }
+    int counter=1;
+    while(counter<n-1){ 
+            //n-1 iterations before sorting
+    for(int i=0;i<n-counter;i++){
+        if(array[i]>array[i+1]){
+            int temp=array[i];
+            array[i]=array[i+1];
+            array[i+1]=temp;
+        }
+    }
+    counter++;
+
+
+    }
+    for(int i=0;i<n;i++){
+        cout<<array[i]<<"  ";
+    }
+    return 0;
+}
