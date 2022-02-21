1d136e97e3338abec61513ba524b240811ec2852
avneet <avneetk1312@gmail.com>
2022-02-21 22:51:16 +0530

    array pointer and palindrome

---
+++ array/array_pointer.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={10,20,30};
+    cout<<*arr<<endl;   //*arr means the first element 
+                        // arr means the adress of first elemnt 
+   
+   int *ptr=arr;        // *ptr gives arr first element and so on 
+                        // ptr = adress as *ptr=arr so ptr=&arr
+                        // &arr gives adress of array 
+   for (int i=0;i<3;i++){
+       cout<<*ptr<<endl;
+       ptr++;
+   }
+}
--- array/palindrome.cpp
+++ array/palindrome.cpp
@@ -9,7 +9,7 @@ int main(){
     bool check=1;
 
     for(int i=0;i<n;i++){
-        if(arr[i] != arr[n-1-i]){
+        if(arr[i] != arr[n-1-i]){  //n-1-i means to go till n-2,3,4,5.....
             check=0;
             break;
         }
