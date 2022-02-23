bd8723f5c37a4f47d3ccab15a521e7667165f3e7
avneet <avneetk1312@gmail.com>
2022-02-23 21:43:18 +0530

    clear bit

--- array/getbit.cpp
+++ array/getbit.cpp
@@ -6,7 +6,11 @@ int getbit(int n, int pos){
 int setbit(int n, int pos){
     return (n | (1<<pos));
 }
+int clearbit(int n,int pos){
+    return  (n & (~(1<<pos)));
+}
 int main(){
     //cout<<getbit(5,2);
-    cout<<setbit(5,1);
+    //cout<<setbit(5,1);
+    cout<<clearbit(5,2);
 }
