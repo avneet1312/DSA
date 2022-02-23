aa44623203b161a56dab6334d84d5b37ffb1f315
avneet <avneetk1312@gmail.com>
2022-02-23 21:31:25 +0530

    set bit

--- array/getbit.cpp
+++ array/getbit.cpp
@@ -3,6 +3,10 @@ using namespace std;
 int getbit(int n, int pos){
     return ((n&(1<<pos)) != 0);
 }
+int setbit(int n, int pos){
+    return (n | (1<<pos));
+}
 int main(){
-    cout<<getbit(5,2);
+    //cout<<getbit(5,2);
+    cout<<setbit(5,1);
 }
