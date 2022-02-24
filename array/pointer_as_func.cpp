cda4a1124759454c75accb13e8aa9410e320bd0c
avneet <avneetk1312@gmail.com>
2022-02-24 22:46:30 +0530

    sieve of eratostheness gives you the prime numbers in  a given range

--- array/pointer_as_func.cpp
+++ array/pointer_as_func.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-void swap(int *a, int *b){
+void swap(int *a, int *b){      //call by reference
     int temp=*a;
     *a=*b;
     *b=temp;
@@ -9,6 +9,6 @@ int main(){
     int a=2;
     int b=4;
 
-     swap(&a,&b);
+     swap(&a,&b);   //passing pointrs in func
      cout<<a<<"  "<<b<<endl;
 }
